8d2d0cf9fe8f18579637b8ec69671c85c60ed00b
Ty Aponte <aaustinsk8@aol.com>
2023-03-01 20:49:59 -0800

    removed node mods

--- clients/driver/driver.js
+++ clients/driver/driver.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const { io } = require('socket.io-client');
-const socket = io('http://localhost:3001/caps');
+// const { io } = require('socket.io-client');
+// const socket = io('http://localhost:3001/caps');
 
 
-module.exports = (payload) => {
-  console.log(`DRIVER: picked up ${payload.orderID}`);
-  eventPool.emit('in-transit', payload);
+// module.exports = (payload) => {
+//   console.log(`DRIVER: picked up ${payload.orderID}`);
+//   eventPool.emit('in-transit', payload);
 
-};
+// };
--- clients/driver/index.js
+++ clients/driver/index.js
@@ -1,15 +1,19 @@
 'use strict';
 
 
-const handler = require('./driver');
+// const handler = require('./driver');
 const { io } = require('socket.io-client');
 
 const socket = io.connect('http://localhost:3001/caps');
 
+socket.emit('getAll', {queueId: ' DRIVER'});
+
 socket.on('pickup', (payload) => {
   setTimeout(() => {
-    handler(payload);
-  }, 1000);
+    console.log(`DRIVER: picked up ${payload.orderID}`);
+    socket.emit('received', { queueId: 'DRIVER' });
+    socket.emit('in-transit', payload);
+  },1000);
 });
 
 socket.on('in-transit', (payload) => {
